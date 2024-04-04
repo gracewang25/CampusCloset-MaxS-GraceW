703b9cd8cfb2a885a1de791d98ae50729a2e0dcb
Grace Wang <grace25@bu.edu>
2024-04-03 23:18:52 -0400

    added primitive loading and login views and loading transition logic

--- CampusClosetMaxSGraceW/app/src/main/java/com/example/campuscloset_maxs_gracew/LoginActivity.kt
+++ CampusClosetMaxSGraceW/app/src/main/java/com/example/campuscloset_maxs_gracew/LoginActivity.kt
@@ -1,28 +1,20 @@
 package com.example.campuscloset_maxs_gracew
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.logging.Handler
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        supportActionBar?.hide()
-        android.os.Handler().postDelayed({
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }, 3000)
     }
 }
