1c1ecfafdf26a4edcc19d360b40c8ff707e658d2
Maxim Slobodchikov <maxims@bu.edu>
2024-05-09 09:14:01 -0400

    retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
     implementation("com.google.android.gms:play-services-location:17.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
